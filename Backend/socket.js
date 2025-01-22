bcf00bf7bc9e1d80a1659780a2ec2d63544e5863
Aditya <aditya2012058@akgec.ac.in>
2025-01-22 22:07:58 +0530

    Implement socket.io integration for real-time user updates and location tracking

---
+++ Backend/socket.js
@@ -0,0 +1,55 @@
+const socketIo = require("socket.io");
+const userModel = require("./model/user.model");
+const captainModel = require("./model/captain.model");
+
+let io;
+
+function initialiseSocket(server) {
+  io = socketIo(server, {
+    cors: { origin: "*", methods: ["GET", "POST"] },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("join", async (data) => {
+      const { userId, userType } = data;
+
+      if (userType === "user") {
+        await userModel.findByIdAndUpdate(userId, { socketId: socket.id });
+      } else if (userType === "captain") {
+        await captainModel.findByIdAndUpdate(userId, { socketId: socket.id });
+      }
+
+      console.log(`User ${userId} joined with socketId: ${socket.id}`);
+    });
+
+    socket.on("update-location-captain", async (data) => {
+      const { userId, location } = data;
+
+      if (!location || !location.ltd || !location.lng) {
+        return socket.emit("error", "Invalid location");
+      }
+      await captainModel.findByIdAndUpdate(userId, {
+        location: {
+          ltd: location.ltd,
+          lng: location.lng,
+        },
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`User disconnected: ${socket.id}`);
+    });
+  });
+}
+
+function sendMessageToSocketId(socketId, messageObject) {
+  if (io) {
+    io.to(socketId).emit(messageObject.event, messageObject.data);
+  } else {
+    console.error("Socket.io not initialized.");
+  }
+}
+
+module.exports = { initialiseSocket, sendMessageToSocketId };
